c6acd500cf17c89cce19c637bb45ae7c613dacc1
Hamza Amjad <hamzaharis298@gmail.com>
2024-07-14 20:46:55 +0500

    Add files via upload

---
+++ New folder (3)/min.ts
@@ -0,0 +1,31 @@
+// let myname = "hamza"
+// console.log(myname);
+// setTimeout(() => {
+//     console.log(myname)
+// },7000);
+// let myage = 45
+// console.log("myage");
+// setTimeout(() => {
+//     console.log("myage")
+// },5000);
+// let myname1 = "ali"
+// console.log("myname1");
+// setTimeout(() => {
+//     console.log("myname1")
+// },1000);
+// function order(){
+//     setTimeout(() => {
+//         console.log("please wait");
+        
+//     },4000);
+// }
+// order()
+// //call back
+
+//  function prepare() {
+//     setTimeout(() => {
+//        console.log("please wait to pizza");
+        
+//     }, 4000);
+//  }
+ 
